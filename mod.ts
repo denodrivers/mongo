d2a0d88e1e1500e9c939b2918d399daeea9b8336
Enok <416828041@qq.com>
2020-04-01 19:01:52 +0800

    release v0.5.0

--- mod.ts
+++ mod.ts
@@ -4,5 +4,5 @@ export * from "./ts/database.ts";
 export * from "./ts/result.ts";
 export { ObjectId } from "./ts/types.ts";
 export * from "./ts/util.ts";
-export const VERSION = "v0.4.0";
+export const VERSION = "v0.5.0";
 export const RELEASE_URL = `https://github.com/manyuanrong/deno_mongo/releases/download/${VERSION}`;
