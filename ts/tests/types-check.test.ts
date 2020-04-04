8edaaaf8aefc9877476d63980ac5648bbbf681b0
Andreas Hippler <andreas@tabaro.de>
2020-04-04 15:58:05 +0200

    fix test deps

--- ts/tests/types-check.test.ts
+++ ts/tests/types-check.test.ts
@@ -1,7 +1,9 @@
-import { assertThrows } from "https://deno.land/std@v0.35.0/testing/asserts.ts";
+import { assertEquals, assertThrows } from "../../test.deps.ts";
 import { ObjectId } from "../types.ts";
 
 Deno.test("ObjectId parse", function() {
-  assertThrows(() => ObjectId("d3e48b03-7baf-4d98-8df7-5002a8fae5e4"));
-  ObjectId("5e63a91b00d839ae0084dfe4");
+  let $oid = "d3e48b03-7baf-4d98-8df7-5002a8fae5e4";
+  assertThrows(() => ObjectId($oid));
+  $oid = "5e63a91b00d839ae0084dfe4";
+  assertEquals(ObjectId($oid), { $oid });
 });
