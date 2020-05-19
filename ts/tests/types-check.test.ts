006c41eb3df8629022a1c6304c847cf3d76b0eb4
Jasper van Haastert <jasper@jbarzilaij.nl>
2020-05-19 11:16:24 +0200

    Fix formatting

--- ts/tests/types-check.test.ts
+++ ts/tests/types-check.test.ts
@@ -6,7 +6,7 @@ const { test } = Deno;
 test("ObjectId parse", () => {
   let $oid = "d3e48b03-7baf-4d98-8df7-5002a8fae5e4";
   assertThrows(() => ObjectId($oid));
-  
+
   $oid = "5e63a91b00d839ae0084dfe4";
   assertEquals(ObjectId($oid), { $oid });
 
