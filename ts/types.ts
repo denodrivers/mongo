7c42f82a9d5f285608e305a2e75be76d8f895288
Jasper van Haastert <jasper@jbarzilaij.nl>
2020-05-19 15:50:26 +0200

    Fix ObjectId regex and add tests for invalid ids (#29)
    【Fix】ObjectId regex and add tests for invalid ids

--- ts/types.ts
+++ ts/types.ts
@@ -17,7 +17,7 @@ export interface ObjectId {
   $oid: string;
 }
 export function ObjectId($oid: string) {
-  const isLegal = /[0-9a-fA-F]{24}/.test($oid);
+  const isLegal = /^[0-9a-fA-F]{24}$/.test($oid);
   if (!isLegal) {
     throw new Error(`ObjectId("${$oid}") is not legal.`);
   }
