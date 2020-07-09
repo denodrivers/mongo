285a843725c0367efc3d81c89d47b72244b272e2
Wilfred Lopez <cubamc@gmail.com>
2020-07-09 15:48:08 -0400

    improved typecript support for collection class (minor change)

--- ts/collection.ts
+++ ts/collection.ts
@@ -1,17 +1,44 @@
 import { MongoClient } from "./client.ts";
 import { UpdateResult } from "./result.ts";
-import { CommandType, FindOptions } from "./types.ts";
+import { CommandType, FindOptions, ObjectId } from "./types.ts";
 import { convert, parse } from "./type_convert.ts";
 import { dispatchAsync, encode } from "./util.ts";
 
-export class Collection<T = any> {
+/**
+ * 
+ * @example
+ * import { MongoClient } from "https://deno.land/x/mongo/mod.ts";
+ * import { DATABASE_NAME, MONGO_URL } from "./constants.ts";
+ * const client = new MongoClient();
+ * 
+ * client.connectWithUri(
+ *   `${MONGO_URL}/${DATABASE_NAME}`,
+ * );
+ * 
+ * const db = client.database(DATABASE_NAME);
+ * 
+ * export interface Answer {
+ *   surveyId: string;
+ *   date: Date;
+ *   userAgent: string;
+ *   answers: { [key: string]: string | string[] | null };
+ * }
+ * 
+ * //Using Generics to get better typescript completition.
+ * export const answersCollection = db.collection<Answer>("answers");
+ * 
+ * //Typecript will infer type: const answers: Answer[]
+ * const answers = await answersCollection.find();
+ * 
+ */
+export class Collection<T extends {}> {
   constructor(
     private readonly client: MongoClient,
     private readonly dbName: string,
     private readonly collectionName: string,
   ) {}
 
-  private async _find(filter?: Object, options?: FindOptions): Promise<any> {
+  private async _find(filter?: Partial<T>, options?: FindOptions): Promise<T> {
     const doc = await dispatchAsync(
       {
         command_type: CommandType.Find,
@@ -26,10 +53,10 @@ export class Collection<T = any> {
         }),
       ),
     );
-    return doc;
+    return doc as T;
   }
 
-  public async count(filter?: Object): Promise<number> {
+  public async count(filter?: Partial<T>): Promise<number> {
     const count = await dispatchAsync(
       {
         command_type: CommandType.Count,
@@ -46,15 +73,15 @@ export class Collection<T = any> {
     return count as number;
   }
 
-  public async findOne(filter?: Object): Promise<T> {
+  public async findOne(filter?: Partial<T>): Promise<T> {
     return parse(await this._find(filter, { findOne: true }));
   }
 
-  public async find(filter?: Object, options?: FindOptions): Promise<T[]> {
+  public async find(filter?: Partial<T>, options?: FindOptions): Promise<T[]> {
     return parse(await this._find(filter, { findOne: false, ...options }));
   }
 
-  public async insertOne(doc: Partial<T>): Promise<any> {
+  public async insertOne(doc: T): Promise<ObjectId> {
     const _id = await dispatchAsync(
       {
         command_type: CommandType.InsertOne,
@@ -68,10 +95,10 @@ export class Collection<T = any> {
         }),
       ),
     );
-    return _id;
+    return _id as ObjectId;
   }
 
-  public async insertMany(docs: Partial<T>[]): Promise<any> {
+  public async insertMany(docs: T[]): Promise<ObjectId[]> {
     const _ids = await dispatchAsync(
       {
         command_type: CommandType.InsertMany,
@@ -85,11 +112,11 @@ export class Collection<T = any> {
         }),
       ),
     );
-    return _ids;
+    return _ids as ObjectId[];
   }
 
   private async _delete(
-    query: Object,
+    query: Partial<T>,
     deleteOne: boolean = false,
   ): Promise<number> {
     const deleteCount = await dispatchAsync(
@@ -109,19 +136,19 @@ export class Collection<T = any> {
     return deleteCount as number;
   }
 
-  public deleteOne(query: Object): Promise<number> {
+  public deleteOne(query: Partial<T>): Promise<number> {
     return this._delete(query, true);
   }
 
-  public deleteMany(query: Object): Promise<number> {
+  public deleteMany(query: Partial<T>): Promise<number> {
     return this._delete(query, false);
   }
 
   private async _update(
-    query: Object,
-    update: Object,
+    query: Partial<T>,
+    update: Partial<T>,
     updateOne: boolean = false,
-  ): Promise<UpdateResult> {
+  ): Promise<UpdateResult & T> {
     const result = await dispatchAsync(
       {
         command_type: CommandType.Update,
@@ -137,18 +164,24 @@ export class Collection<T = any> {
         }),
       ),
     );
-    return result as UpdateResult;
+    return result as UpdateResult & T;
   }
 
-  public updateOne(query: Object, update: Object): Promise<UpdateResult> {
+  public updateOne(
+    query: Partial<T>,
+    update: Partial<T>,
+  ): Promise<UpdateResult & T> {
     return this._update(query, update, true);
   }
 
-  public updateMany(query: Object, update: Object): Promise<UpdateResult> {
+  public updateMany(
+    query: Partial<T>,
+    update: Partial<T>,
+  ): Promise<UpdateResult & T> {
     return this._update(query, update, false);
   }
 
-  public async aggregate(pipeline: Object[]): Promise<T[]> {
+  public async aggregate<T = any>(pipeline: Object[]): Promise<T[]> {
     const docs = await dispatchAsync(
       {
         command_type: CommandType.Aggregate,
@@ -162,7 +195,7 @@ export class Collection<T = any> {
         }),
       ),
     );
-    return parse(docs);
+    return parse(docs) as T[];
   }
 
   public async createIndexes(
