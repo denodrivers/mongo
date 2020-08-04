fc5fc1407feb6b87b21eab6435b570b0e78b5457
Luca Casonato <lucacasonato@yahoo.com>
2020-08-04 16:52:10 +0200

    Update plugin_prepare to 0.7.2

--- test.deps.ts
+++ test.deps.ts
@@ -1,7 +1,7 @@
-export { exists } from "https://deno.land/std@0.61.0/fs/mod.ts";
+export { exists } from "https://deno.land/std@0.63.0/fs/mod.ts";
 export {
   assert,
   assertEquals,
   assertThrows,
   assertThrowsAsync,
-} from "https://deno.land/std@0.61.0/testing/asserts.ts";
+} from "https://deno.land/std@0.63.0/testing/asserts.ts";
