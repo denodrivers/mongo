2c4a037dd8070ea2124287d71bfe2553080a4080
Makuza Mugabo Verite <mugaboverite@gmail.com>
2020-08-22 11:19:54 +0200

    Fixed error where to require a file was using version 0.10.1 hence donwload v0.10.0

--- mod.ts
+++ mod.ts
@@ -6,8 +6,7 @@ export * from "./ts/database.ts";
 export * from "./ts/result.ts";
 export { ObjectId, UpdateOptions } from "./ts/types.ts";
 export * from "./ts/util.ts";
-export const VERSION = "v0.10.0";
-export const RELEASE_URL =
-  `https://github.com/manyuanrong/deno_mongo/releases/download/${VERSION}`;
+export const VERSION = "v0.10.1";
+export const RELEASE_URL = `https://github.com/manyuanrong/deno_mongo/releases/download/${VERSION}`;
 
 await init(RELEASE_URL);
