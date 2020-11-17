9381f89523b37ca3924d604fb6e414239161e677
silence_zhpf <silence_zhpf@aliyun.com>
2020-11-17 11:06:56 +0800

    feature(auth): 添加 HI 方法
    :sparkles: 添加 HI 方法
    :white_check_mark: 添加 HI 的测试用例

--- deps.ts
+++ deps.ts
@@ -1,5 +1,6 @@
 export * as Bson from "./bson/mod.ts";
 export { createHash } from "https://deno.land/std@0.77.0/hash/mod.ts";
+export { pbkdf2Sync } from "https://deno.land/std@0.78.0/node/_crypto/pbkdf2.ts";
 export * from "https://deno.land/x/bytes_formater/mod.ts";
 export { BufReader } from "https://deno.land/std@0.77.0/io/mod.ts";
 export { deferred } from "https://deno.land/std@0.77.0/async/deferred.ts";
--- src/auth/mod.ts
+++ src/auth/mod.ts
@@ -1,7 +1,10 @@
-import {createHash} from '../../deps.ts';
+import { createHash, pbkdf2Sync } from "../../deps.ts";
 
-export function passwordDigest(username:string, password:string):string{
-  const hash = createHash('md5');
+export function passwordDigest(username: string, password: string): string {
+  const hash = createHash("md5");
   hash.update(`${username}:mongo:${password}`);
   return hash.toString();
+}
+export function HI(data: string, salt: string, iterations: number) {
+  return pbkdf2Sync(data, salt, iterations, 20, "sha1");
 }
