197b25f604547f6ffcdc9a7a067f5602eabf9b39
hviana <hv5088@gmail.com>
2021-01-07 23:39:08 -0300

    Authorization tests

--- tests/connect.test.ts
+++ tests/connect.test.ts
@@ -3,7 +3,7 @@ import { MongoClient } from "../src/client.ts";
 
 const hostname = "127.0.0.1";
 
-Deno.test("testConnectWithUri", async () => {
+Deno.test("test connect", async () => {
   const client = new MongoClient();
   await client.connect(`mongodb://${hostname}:27017`);
   const names = await client.listDatabases();
@@ -12,12 +12,35 @@ Deno.test("testConnectWithUri", async () => {
   client.close();
 });
 
-Deno.test("testConnectWithOptions", async () => {
+Deno.test("testconnect With Options", async () => {
   const client = new MongoClient();
   await client.connect({
     servers: [{ host: hostname, port: 27017 }],
     db: "admin",
   });
+  const names = await client.listDatabases();
+  assert(names instanceof Array);
+  assert(names.length > 0);
+  client.close();
+});
+
+Deno.test("connect authorization test 1 - admin db", async () => {
+  var username = "user1";
+  var password = "y3mq3mpZ3J6PGfgg";
+  const client = new MongoClient();
+  await client.connect(`mongodb://${username}:${password}@${hostname}:27017`);
+  const names = await client.listDatabases();
+  console.log(names);
+  assert(names instanceof Array);
+  assert(names.length > 0);
+  client.close();
+});
+
+Deno.test("connect authorization test 2 - admin db", async () => {
+  var username = "user2";
+  var password = "Qa6WkQSuXF425sWZ";
+  const client = new MongoClient();
+  await client.connect(`mongodb://${username}:${password}@${hostname}:27017`);
   const names = await client.listDatabases();
   assert(names instanceof Array);
   assert(names.length > 0);
