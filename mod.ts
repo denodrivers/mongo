b444915bafd38aafb577605adadeb6b7d92c272e
RobertFent <robert.fent@live.com>
2021-05-24 08:32:47 +0200

    added missing semicolon

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 export { MongoClient } from "./src/client.ts";
 export { Database } from "./src/database.ts";
-export { Collection } from "./src/collection/mod.ts"
+export { Collection } from "./src/collection/mod.ts";
 export * from "./src/types.ts";
 export { Bson } from "./deps.ts";
