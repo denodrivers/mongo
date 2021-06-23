a3ef0bdc5a94eee8cb7e4836e44ac962344d4487
buttercubz <ericksosagarcias@gmail.com>
2021-06-23 10:41:09 -0400

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,14 +1,14 @@
 export * as Bson from "./bson/mod.ts";
-export { createHash } from "https://deno.land/std@0.83.0/hash/mod.ts";
-export { pbkdf2Sync } from "https://deno.land/std@0.83.0/node/_crypto/pbkdf2.ts";
-export { HmacSha1 } from "https://deno.land/std@0.83.0/hash/sha1.ts";
-export { HmacSha256 } from "https://deno.land/std@0.83.0/hash/sha256.ts";
-export * from "https://deno.land/x/bytes_formater/mod.ts";
-export { BufReader } from "https://deno.land/std@0.83.0/io/mod.ts";
-export { deferred } from "https://deno.land/std@0.83.0/async/deferred.ts";
-export type { Deferred } from "https://deno.land/std@0.83.0/async/deferred.ts";
-export * as b64 from "https://deno.land/std@0.83.0/encoding/base64.ts";
+export { createHash } from "https://deno.land/std@0.99.0/hash/mod.ts";
+export { pbkdf2Sync } from "https://deno.land/std@0.99.0/node/_crypto/pbkdf2.ts";
+export { HmacSha1 } from "https://deno.land/std@0.99.0/hash/sha1.ts";
+export { HmacSha256 } from "https://deno.land/std@0.99.0/hash/sha256.ts";
+export * from "https://deno.land/x/bytes_formater@v1.4.0/mod.ts";
+export { BufReader } from "https://deno.land/std@0.99.0/io/mod.ts";
+export { deferred } from "https://deno.land/std@0.99.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.99.0/async/deferred.ts";
+export * as b64 from "https://deno.land/std@0.99.0/encoding/base64.ts";
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.83.0/testing/asserts.ts";
+} from "https://deno.land/std@0.99.0/testing/asserts.ts";
