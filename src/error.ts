6732b99fafbb8e87337fde88b38b5a09c2fd9421
Manuel Aguirre <aguirre.manuel7@gmail.com>
2021-08-27 08:09:05 -0300

    made info in MongoError class accessible

--- src/error.ts
+++ src/error.ts
@@ -5,8 +5,18 @@ export interface MongoErrorInfo {
   codeName: string;
 }
 
-export class MongoError extends Error {
-  constructor(info: MongoErrorInfo | string) {
+export class MongoError extends Error implements MongoErrorInfo {
+  ok: 0;
+  errmsg: string;
+  code: number;
+  codeName: string;
+
+  constructor(info: MongoErrorInfo) {
     super(`MongoError: ${JSON.stringify(info)}`);
+
+    this.ok = info.ok;
+    this.errmsg = info.errmsg;
+    this.code = info.code;
+    this.codeName = info.codeName;
   }
 }
