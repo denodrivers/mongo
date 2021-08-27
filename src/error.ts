909b3882edf9a81f0a88353479e09f80686636ee
Manuel Aguirre <aguirre.manuel7@gmail.com>
2021-08-27 14:31:20 -0300

    added JSdoc

--- src/error.ts
+++ src/error.ts
@@ -1,3 +1,7 @@
+/**
+ * Representation of a MongoDB server error response.
+ * @public
+ */
 export interface MongoErrorInfo {
   ok: 0;
   errmsg: string;
@@ -5,24 +9,44 @@ export interface MongoErrorInfo {
   codeName: string;
 }
 
+/**
+ * A base class from which Mongo errors are derived.
+ * @public
+ */
 export abstract class MongoError extends Error {
   constructor(info: MongoErrorInfo | string) {
     super(`MongoError: ${JSON.stringify(info)}`);
   }
 }
 
+/**
+ * A class representation of an error ocurring during the driver's execution.
+ * @public
+ */
 export class MongoDriverError extends MongoError {
+  /**
+   *
+   * @param info A string containing the error's message.
+   */
   constructor(info: string) {
     super(info);
   }
 }
 
+/**
+ * A class representation of an error returned by MongoDB server.
+ * @public
+ */
 export class MongoServerError extends MongoError implements MongoErrorInfo {
   ok: 0;
   errmsg: string;
   code: number;
   codeName: string;
 
+  /**
+   *
+   * @param info An object representing the server's error response.
+   */
   constructor(info: MongoErrorInfo) {
     super(info);
 
