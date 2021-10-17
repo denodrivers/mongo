2979072152416d016c23508818f8ffa350b57371
Erfan Safari <erfanshield@outlook.com>
2021-10-17 12:12:41 +0330

    chore: remove unused dependency

--- deps.ts
+++ deps.ts
@@ -5,7 +5,6 @@ export { createHash } from "https://deno.land/std@0.110.0/hash/mod.ts";
 export { pbkdf2Sync } from "https://deno.land/std@0.110.0/node/_crypto/pbkdf2.ts";
 export { HmacSha1 } from "https://deno.land/std@0.110.0/hash/sha1.ts";
 export { HmacSha256 } from "https://deno.land/std@0.110.0/hash/sha256.ts";
-export * from "https://deno.land/x/bytes_formater@v1.4.0/mod.ts";
 export { BufReader, writeAll } from "https://deno.land/std@0.110.0/io/mod.ts";
 export { deferred } from "https://deno.land/std@0.110.0/async/deferred.ts";
 export type { Deferred } from "https://deno.land/std@0.110.0/async/deferred.ts";
