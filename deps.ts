251074a77d0ccee03f9b10f3704d1bd7277d6dab
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2021-10-23 21:07:44 +0800

    Deps: bump `std` to `0.113.0`

--- deps.ts
+++ deps.ts
@@ -1,13 +1,12 @@
 export * as Bson from "https://cdn.skypack.dev/bson@4.5.3?dts";
-export type { Document } from "./bson/mod.ts";
-export { createHash } from "https://deno.land/std@0.111.0/hash/mod.ts";
-export { HmacSha1 } from "https://deno.land/std@0.111.0/hash/sha1.ts";
-export { HmacSha256 } from "https://deno.land/std@0.111.0/hash/sha256.ts";
-export { BufReader, writeAll } from "https://deno.land/std@0.111.0/io/mod.ts";
-export { deferred } from "https://deno.land/std@0.111.0/async/deferred.ts";
-export type { Deferred } from "https://deno.land/std@0.111.0/async/deferred.ts";
-export * as b64 from "https://deno.land/std@0.111.0/encoding/base64.ts";
+export { createHash } from "https://deno.land/std@0.113.0/hash/mod.ts";
+export { HmacSha1 } from "https://deno.land/std@0.113.0/hash/sha1.ts";
+export { HmacSha256 } from "https://deno.land/std@0.113.0/hash/sha256.ts";
+export { BufReader, writeAll } from "https://deno.land/std@0.113.0/io/mod.ts";
+export { deferred } from "https://deno.land/std@0.113.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.113.0/async/deferred.ts";
+export * as b64 from "https://deno.land/std@0.113.0/encoding/base64.ts";
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.111.0/testing/asserts.ts";
+} from "https://deno.land/std@0.113.0/testing/asserts.ts";
