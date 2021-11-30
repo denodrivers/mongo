530ef7da5ff168b3c97ce242bc22b6271e42c2f3
Erfan Safari <erfanshield@outlook.com>
2021-11-30 11:36:50 +0330

    chore(deps): drop std/hash (#301)
    * chore(deps): drop std/hash
    * bump(deps) update std

--- deps.ts
+++ deps.ts
@@ -1,13 +1,12 @@
 export * as Bson from "./bson/bson.ts";
-export { createHash } from "https://deno.land/std@0.115.1/hash/mod.ts";
-export { HmacSha1 } from "https://deno.land/std@0.115.1/hash/sha1.ts";
-export { HmacSha256 } from "https://deno.land/std@0.115.1/hash/sha256.ts";
-export { BufReader } from "https://deno.land/std@0.115.1/io/mod.ts";
-export { writeAll } from "https://deno.land/std@0.115.1/streams/conversion.ts";
-export { deferred } from "https://deno.land/std@0.115.1/async/deferred.ts";
-export type { Deferred } from "https://deno.land/std@0.115.1/async/deferred.ts";
-export * as b64 from "https://deno.land/std@0.115.1/encoding/base64.ts";
+export { crypto } from "https://deno.land/std@0.116.0/crypto/mod.ts";
+export { BufReader } from "https://deno.land/std@0.116.0/io/mod.ts";
+export { writeAll } from "https://deno.land/std@0.116.0/streams/conversion.ts";
+export { deferred } from "https://deno.land/std@0.116.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.116.0/async/deferred.ts";
+export * as b64 from "https://deno.land/std@0.116.0/encoding/base64.ts";
+export * as hex from "https://deno.land/std@0.116.0/encoding/hex.ts";
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.115.1/testing/asserts.ts";
+} from "https://deno.land/std@0.116.0/testing/asserts.ts";
--- tests/cases/01_auth.ts
+++ tests/cases/01_auth.ts
@@ -33,8 +33,8 @@ export default function authTests() {
   for (const { username, password, digest } of passwordValids) {
     Deno.test({
       name: `passwordDigest:${username}:${password}`,
-      fn() {
-        const digestRes: string = passwordDigest(username, password);
+      async fn() {
+        const digestRes: string = await passwordDigest(username, password);
         assertEquals(digestRes, digest);
       },
     });
@@ -95,7 +95,7 @@ export default function authTests() {
         "sha1",
       );
       assertEquals(
-        saltedPassword,
+        new Uint8Array(saltedPassword),
         Uint8Array.from([
           72,
           84,
