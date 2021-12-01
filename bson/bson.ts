615f52daba39c9642fa5d0c49af81b08abe2ba5c
Erfan Safari <erfanshield@outlook.com>
2021-12-01 13:45:04 +0330

    chore(deps): use pinned url for bson

--- bson/bson.ts
+++ bson/bson.ts
@@ -1,2 +1,2 @@
 // @deno-types='./bson.d.ts'
-export * from "https://cdn.skypack.dev/bson@v4.5.4";
+export * from "https://cdn.skypack.dev/pin/bson@v4.5.4-F38JF4sWndM08oQ5WCDI/mode=imports/optimized/bson.js";
