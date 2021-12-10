1c378901cea3d1a05a1955ffae11a10df470bb76
wafuwafu13 <mariobaske@i.softbank.jp>
2021-12-10 19:33:10 +0900

    chore: fmt

--- tests/test.deps.ts
+++ tests/test.deps.ts
@@ -1,7 +1,7 @@
 export {
   assert,
   assertEquals,
-  assertThrows,
   assertRejects,
+  assertThrows,
 } from "https://deno.land/std@0.117.0/testing/asserts.ts";
 export * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
