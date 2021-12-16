545ea55fa4459e53afba1dfc764dbf76091b751c
lucsoft <mail@lucsoft.de>
2021-12-17 00:31:05 +0100

    fix(tests): Fixed flacky worker test case

--- tests/cases/07_worker.ts
+++ tests/cases/07_worker.ts
@@ -3,27 +3,19 @@ import { assert } from "../test.deps.ts";
 export default function workerTests() {
   Deno.test({
     name: "WORKER: Deno does not throw when deno_mongo is imported in worker",
-    fn: async function () {
-      let workerFinished: (p: void | PromiseLike<void>) => void;
-
-      const p = new Promise<void>((resolve, _reject) => {
-        workerFinished = resolve;
-      });
-
+    fn: async () => {
       const importWorker = new Worker(
         new URL("import_worker.ts", import.meta.url).href,
         { type: "module" },
       );
 
-      importWorker.onmessage = (_e) => {
-        workerFinished();
-      };
-
       importWorker.postMessage("startWorker");
-
-      await p;
-
-      assert(true);
+      await new Promise<void>((done) => {
+        importWorker.onmessage = (_e) => {
+          assert(true);
+          done();
+        };
+      });
     },
   });
 }
