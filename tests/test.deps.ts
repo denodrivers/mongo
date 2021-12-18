60823bd570c45620c3adb7116beb16c28d938dc2
Erfan Safari <erfanshield@outlook.com>
2021-12-18 15:28:02 +0330

    chore(deps): use web_bson for bson library (#317)

--- tests/test.deps.ts
+++ tests/test.deps.ts
@@ -3,6 +3,6 @@ export {
   assertEquals,
   assertRejects,
   assertThrows,
-} from "https://deno.land/std@0.117.0/testing/asserts.ts";
+} from "https://deno.land/std@0.118.0/testing/asserts.ts";
 export * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
 export { deferred } from "https://deno.land/std/async/mod.ts";
