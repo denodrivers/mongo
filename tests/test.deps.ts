0846ad96a83690aa685fcaad1f0234ef20517779
ERFANIUM <erfanshield@outlook.com>
2021-12-19 13:56:56 +0330

    chore(deps): add udd.yml to github workflows

--- tests/test.deps.ts
+++ tests/test.deps.ts
@@ -5,4 +5,3 @@ export {
   assertThrows,
 } from "https://deno.land/std@0.118.0/testing/asserts.ts";
 export * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
-export { deferred } from "https://deno.land/std/async/mod.ts";
