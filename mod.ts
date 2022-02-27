1964893a184bca1894219e91b320e64210ed92c7
lucsoft <mail@lucsoft.de>
2022-02-27 17:47:00 +0100

    refactor: made non breaking

--- mod.ts
+++ mod.ts
@@ -2,5 +2,22 @@ export { MongoClient } from "./src/client.ts";
 export { Database } from "./src/database.ts";
 export { Collection } from "./src/collection/mod.ts";
 export * from "./src/types.ts";
-export * from "https://deno.land/x/web_bson@v0.1.9/mod.ts";
+export * as Bson from "./deps.ts";
+export {
+  Binary,
+  BSONRegExp,
+  BSONSymbol,
+  Code,
+  DBRef,
+  Decimal128,
+  Double,
+  Int32,
+  Long,
+  MaxKey,
+  MinKey,
+  ObjectId,
+  Timestamp,
+  UUID,
+} from "./deps.ts";
+export type { Document } from "./deps.ts";
 export { GridFSBucket } from "./src/gridfs/bucket.ts";
