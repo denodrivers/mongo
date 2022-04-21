1c3dd0e292082b271a5a8ffe3779e1cb6dd79b21
ERFANIUM <erfanshield@outlook.com>
2022-04-22 02:41:13 +0430

    bump(deps): update dependencies

--- tests/test.deps.ts
+++ tests/test.deps.ts
@@ -4,6 +4,6 @@ export {
   assertNotEquals,
   assertRejects,
   assertThrows,
-} from "https://deno.land/std@0.130.0/testing/asserts.ts";
-export { equals as bytesEquals } from "https://deno.land/std@0.130.0/bytes/equals.ts";
+} from "https://deno.land/std@0.136.0/testing/asserts.ts";
+export { equals as bytesEquals } from "https://deno.land/std@0.136.0/bytes/equals.ts";
 export * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
