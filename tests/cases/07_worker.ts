eb916a491de6476fadd76018da26bf79088e5c1d
ERFANIUM <erfanshield@outlook.com>
2022-07-23 17:20:43 +0430

    improve(test): use import.meta.resolve

--- tests/cases/07_worker.ts
+++ tests/cases/07_worker.ts
@@ -5,7 +5,7 @@ Deno.test({
   name: "WORKER: Deno does not throw when deno_mongo is imported in worker",
   fn: async () => {
     const importWorker = new Worker(
-      new URL("import_worker.ts", import.meta.url).href,
+      import.meta.resolve("./import_worker.ts"),
       { type: "module" },
     );
     const p = deferred<string>();
