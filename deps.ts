3cbe6ebe408f07630fe73140f18620efb9e1ffab
ERFANIUM <erfanshield@outlook.com>
2022-07-23 15:37:57 +0430

    dump: upgrade deps

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export * from "https://deno.land/x/web_bson@v0.2.3/mod.ts";
-export { crypto } from "https://deno.land/std@0.144.0/crypto/mod.ts";
-export { BufReader } from "https://deno.land/std@0.144.0/io/mod.ts";
-export { writeAll } from "https://deno.land/std@0.144.0/streams/conversion.ts";
-export { deferred } from "https://deno.land/std@0.144.0/async/deferred.ts";
-export type { Deferred } from "https://deno.land/std@0.144.0/async/deferred.ts";
-export * as b64 from "https://deno.land/std@0.144.0/encoding/base64.ts";
-export * as hex from "https://deno.land/std@0.144.0/encoding/hex.ts";
+export * from "https://deno.land/x/web_bson@v0.2.4/mod.ts";
+export { crypto } from "https://deno.land/std@0.149.0/crypto/mod.ts";
+export { BufReader } from "https://deno.land/std@0.149.0/io/mod.ts";
+export { writeAll } from "https://deno.land/std@0.149.0/streams/conversion.ts";
+export { deferred } from "https://deno.land/std@0.149.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.149.0/async/deferred.ts";
+export * as b64 from "https://deno.land/std@0.149.0/encoding/base64.ts";
+export * as hex from "https://deno.land/std@0.149.0/encoding/hex.ts";
