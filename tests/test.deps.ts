eb32d514878f08646aa7cd6f13726d6d79d2dd9e
ERFANIUM <erfanshield@outlook.com>
2022-08-09 01:07:16 +0430

    refactor(test): localize test assets

--- tests/test.deps.ts
+++ tests/test.deps.ts
@@ -7,3 +7,7 @@ export {
 } from "https://deno.land/std@0.149.0/testing/asserts.ts";
 export { equals as bytesEquals } from "https://deno.land/std@0.149.0/bytes/equals.ts";
 export * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
+export {
+  readAll,
+  readerFromStreamReader,
+} from "https://deno.land/std@0.149.0/streams/mod.ts";
