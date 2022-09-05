26f110331f2aca42daabc19d1a70af8cb7e6d280
ERFANIUM <erfanshield@outlook.com>
2022-09-05 15:28:05 +0430

    bump dependecies

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export * from "https://deno.land/x/web_bson@v0.2.4/mod.ts";
-export { crypto } from "https://deno.land/std@0.149.0/crypto/mod.ts";
-export { BufReader } from "https://deno.land/std@0.149.0/io/mod.ts";
-export { writeAll } from "https://deno.land/std@0.149.0/streams/conversion.ts";
-export { deferred } from "https://deno.land/std@0.149.0/async/deferred.ts";
-export type { Deferred } from "https://deno.land/std@0.149.0/async/deferred.ts";
-export * as b64 from "https://deno.land/std@0.149.0/encoding/base64.ts";
-export * as hex from "https://deno.land/std@0.149.0/encoding/hex.ts";
+export * from "https://deno.land/x/web_bson@v0.2.5/mod.ts";
+export { crypto } from "https://deno.land/std@0.154.0/crypto/mod.ts";
+export { BufReader } from "https://deno.land/std@0.154.0/io/mod.ts";
+export { writeAll } from "https://deno.land/std@0.154.0/streams/conversion.ts";
+export { deferred } from "https://deno.land/std@0.154.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.154.0/async/deferred.ts";
+export * as b64 from "https://deno.land/std@0.154.0/encoding/base64.ts";
+export * as hex from "https://deno.land/std@0.154.0/encoding/hex.ts";
--- tests/test.deps.ts
+++ tests/test.deps.ts
@@ -4,10 +4,10 @@ export {
   assertNotEquals,
   assertRejects,
   assertThrows,
-} from "https://deno.land/std@0.149.0/testing/asserts.ts";
-export { equals as bytesEquals } from "https://deno.land/std@0.149.0/bytes/equals.ts";
-export * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
+} from "https://deno.land/std@0.154.0/testing/asserts.ts";
+export { equals as bytesEquals } from "https://deno.land/std@0.154.0/bytes/equals.ts";
+export * as semver from "https://deno.land/x/semver@v1.4.1/mod.ts";
 export {
   readAll,
   readerFromStreamReader,
-} from "https://deno.land/std@0.149.0/streams/mod.ts";
+} from "https://deno.land/std@0.154.0/streams/mod.ts";
