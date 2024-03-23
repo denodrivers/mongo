368cce1a1bd291bd2dd71b618fc129fa91f6b5ca
William Horning <wgyt735yt@gmail.com>
2024-03-23 18:15:11 -0400

    whoops i forgot to remove an unused dep

--- deps.ts
+++ deps.ts
@@ -16,7 +16,6 @@ export {
   Timestamp,
   UUID,
 } from "jsr:@lucsoft/web-bson@^0.3.1";
-export {} from "jsr:@std/bytes@^0.220.1/equals";
 export { crypto as stdCrypto } from "jsr:@std/crypto@^0.220.1/crypto";
 export { decodeBase64, encodeBase64 } from "jsr:@std/encoding@^0.220.1/base64";
 export { encodeHex } from "jsr:@std/encoding@^0.220.1/hex";
