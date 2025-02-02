30a073e3439acb856dcb183bac82b045bd67ebc3
Jersey <wgyt735yt@gmail.com>
2025-02-02 16:39:19 -0500

    chore: bump web_bson version

--- deps.ts
+++ deps.ts
@@ -15,7 +15,7 @@ export {
   serialize,
   Timestamp,
   UUID,
-} from "jsr:@lucsoft/web-bson@^0.3.1";
+} from "jsr:@lucsoft/web-bson@^0.4.0";
 export { crypto as stdCrypto } from "jsr:@std/crypto@^1.0.3/crypto";
 export { decodeBase64, encodeBase64 } from "jsr:@std/encoding@^1.0.5/base64";
 export { encodeHex } from "jsr:@std/encoding@^1.0.5/hex";
